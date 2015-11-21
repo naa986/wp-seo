225150ed3f42dc9f5d430607c681dda3bbd1a666
Noor <Noor@noor-pc>
2015-11-21 16:17:20 +1100

    .Removed all languages, some github files and image banners .Renamed the plugin to wp-seo

--- wp-seo/wp-seo.php
+++ wp-seo/wp-seo.php
@@ -4,13 +4,13 @@
  */
 
 /**
- * Plugin Name: WordPress SEO
+ * Plugin Name: WP SEO
  * Version: 2.2.1
  * Plugin URI: https://yoast.com/wordpress/plugins/seo/#utm_source=wpadmin&utm_medium=plugin&utm_campaign=wpseoplugin
  * Description: The first true all-in-one SEO solution for WordPress, including on-page content analysis, XML sitemaps and much more.
  * Author: Team Yoast
  * Author URI: https://yoast.com/
- * Text Domain: wordpress-seo
+ * Text Domain: wp-seo
  * Domain Path: /languages/
  * License: GPL v3
  */
